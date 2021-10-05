dcb85c6a3ce20d34a905a29bc70c701be4af503a
yMarceloMaia <marcelomaiajp@gmail.com>
2021-10-05 16:38:49 -0300

    Adicionei o desafio do dia

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -88,3 +88,89 @@
 // console.log("O primeiro número é igual ao segundo?", primeiroNumeroNumber === segundoNumeroNumber)
 // console.log("O primeiro número é divisível pelo segundo?", (primeiroNumeroNumber % segundoNumeroNumber) === 0)
 // console.log("O segundo número é divisível pelo primeiro?", (segundoNumeroNumber % primeiroNumeroNumber) < 1)
+
+
+
+//DESAFIOS
+//1
+
+//a)
+// let fahrenheit = 77
+// let kelvin = (fahrenheit - 32)*(5/9) + 273.15
+// console.log(kelvin)
+//RESPOSTA: 298.15
+
+//b)
+// let celsius = 80
+// let fahrenheit = celsius * (9/5) + 32
+// console.log(fahrenheit)
+//RESPOTA: 176
+
+//c)
+// let celsius = 30
+// let fahrenheit = celsius * (9/5) + 32
+// let kelvin = (fahrenheit - 32)*(5/9) + 273.15
+// console.log(fahrenheit)
+// console.log(kelvin)
+//RESPOSTA: 86 fahrenheit, 303.15 kelvin
+
+//d)
+//REPOSTA:
+// let celsius = prompt("Qual valor você deseja converter?")
+// let fahrenheit = celsius * (9/5) + 32
+// let kelvin = (fahrenheit - 32)*(5/9) + 273.15
+// console.log(fahrenheit)
+// console.log(kelvin)
+
+
+//2
+
+// let valorQuiloWattHora = 0.05
+// let consumoQuiloWattHora = valorQuiloWattHora * 280
+// console.log(consumoQuiloWattHora)
+//RESPOSTA: a) R$14,00 a ser pago
+// console.log(consumoQuiloWattHora * 0.85)
+//REPOSTA b) com desconto passa a ser R$11,90
+
+
+//3
+//REPOSTA a)
+// let libra = 20
+// let kilo = libra * 0.453592
+// console.log("20lb equivalem a", kilo, "kg")
+//RESPOSTA: 20lb equivalem a 9.07184 kg
+
+//RESPOSTA b)
+// let oz = 10.5
+// let kilo = oz * 0.0283495
+// console.log("10.5oz equivalem a", kilo, "kg")
+// RESPOSTA: 10.5oz equivalem a 0.29766975 kg
+
+//RESPOSTA c)
+// let milha = 100
+// let metro = milha * 1609.34
+// console.log("100mi equivalem a", metro, "m")
+//RESPOSTA: 100mi equivalem a 160934 m
+
+//RESPOSTA d)
+// let pes = 50
+// let metro = pes * 0.3048
+// console.log("50ft equivalem a", metro, "m")
+//REPOSTA: 50ft equivalem a 15.24 m
+
+//RESPOSTA e)
+// let galao = 103.56
+// let litro = galao * 3.78541
+// console.log("103.56gal equivalem a", litro, "l")
+//RESPOSTA: 103.56gal equivalem a 392.01705960000004 l
+
+//RESPOSTA f)
+// let xicara = 450
+// let litro = xicara * 0.284131
+// console.log("450xic equivalem a", litro, "l")
+//RESPOSTA: 450xic equivalem a 127.85895000000001 l
+
+//RESPOSTA g)
+// let libra = prompt("Digite uma quantidade!")
+// let kilo = libra * 0.453592
+// console.log("20lb equivalem a", kilo, "kg")
