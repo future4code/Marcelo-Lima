c773c680fb9c57104c60c8d26fae8c35b0aa76a8
yMarceloMaia <marcelomaiajp@gmail.com>
2021-10-17 20:42:58 -0300

    Feito a parte simples do projeto BlackJack

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,3 +11,26 @@
  * 
  */
 
+// let cartasUsuario = []
+// let cartasComputador = []
+// if(confirm(`Você quer jogar o jogo BlackJack?\nQuer começar uma rodada?`)) {
+//    for(let i = 0; i< 2; i++){
+//       cartasUsuario.push(comprarCarta())
+//       cartasComputador.push(comprarCarta())
+//    }
+//    while(cartasUsuario[0].valor + cartasUsuario[1].valor === 22  && cartasComputador[0].valor + cartasComputador[1].valor === 22){
+//       cartasUsuario = []
+//       cartasComputador = []
+//       for (let i = 0; i < 2; i++){
+//          cartasUsuario.push(comprarCarta())
+//          cartasComputador.push(comprarCarta())
+//       }
+//    }
+//    if(confirm(`Suas cartas são ${cartasUsuario[0].texto} ${cartasUsuario[1].texto}. A carta revelada do computador é ${cartasComputador[0].texto}\nDeseja comprar mais uma carta?`)){
+//       for(let i = 0; i< 1; i++){
+//          cartasUsuario.push(comprarCarta())
+//       } 
+//    } else (alert(`Suas cartas são ${cartasUsuario[0].texto} ${cartasUsuario[1].texto}. sua pontuação é ${cartasUsuario[0].valor + cartasUsuario[1].valor}\nAs cartas do computador são ${cartasComputador[0].texto} ${cartasComputador[1].texto}. A pontuação do computador é ${cartasComputador[0].valor + cartasComputador[1].valor}\n`))
+// }
+
+// console.log(cartasUsuario)
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -11,46 +11,28 @@
  * 
  */
 
-// if(confirm(`Você quer jogar o jogo BlackJack?`)){
-//    console.log(`Boas vindas ao jogo de Blackjack!!!`)
-   
-//    // Se o usuário responder Ok, o programa deve iniciar uma nova rodada. 
-// } else {
-//    console.log(`O jogo acabou!!!`)
-// }
-
-
-// const cartaUsuario = comprarCarta()
-// const cartaComputador = comprarCarta()
-
-// console.log(`Usuário - cartas: ${cartaUsuario.texto} - pontuação:  ${cartaUsuario.valor}`)
-// console.log(`Usuário - cartas: ${cartaComputador.texto} - pontuação:  ${cartaComputador.valor}`)
-
-// if(cartaUsuario.valor > cartaComputador.valor && cartaUsuario.valor < 22) {
-//    console.log(`O usuário venceu!`)
-// } else if (cartaComputador.valor > cartaUsuario.valor && cartaComputador.valor < 22){
-//    console.log(`O computador venceu!`)
-// } else {
-//    console.log(`Empatou!`)
-// }
-
-
-
 if(confirm(`Você quer jogar o jogo BlackJack?`)){
-      console.log(`Boas vindas ao jogo de Blackjack!!!`)
-   
-      // Se o usuário responder Ok, o programa deve iniciar uma nova rodada. 
-      const cartaUsuario = comprarCarta()
-      const cartaComputador = comprarCarta()
+   console.log(`Boas vindas ao jogo de Blackjack!!!`)
 
-      console.log(`Usuário - cartas: ${cartaUsuario.texto} - pontuação:  ${cartaUsuario.valor}`)
-      console.log(`Usuário - cartas: ${cartaComputador.texto} - pontuação:  ${cartaComputador.valor}`)
+   // Se o usuário responder Ok, o programa deve iniciar uma nova rodada. 
+   const carta1Usuario = comprarCarta()
+   const carta1Computador = comprarCarta()
+   const carta2Usuario = comprarCarta()
+   const carta2Computador = comprarCarta()
 
-      if(cartaUsuario.valor > cartaComputador.valor && cartaUsuario.valor < 22) {
-            console.log(`O usuário venceu!`)
-         } else if (cartaComputador.valor > cartaUsuario.valor && cartaComputador.valor < 22){
-            console.log(`O computador venceu!`)
-         } else 
-            console.log(`Empatou!`) 
-} else   {
-   console.log(`O jogo acabou!!!`)}
+   const valorUsuario = carta1Usuario.valor + carta2Usuario.valor
+   const valorComputador = carta1Computador.valor + carta2Computador. valor
+
+   console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - pontuação:  ${valorUsuario}`)
+   console.log(`Computador - cartas: ${carta1Computador.texto} ${carta2Computador.texto} - pontuação:  ${valorComputador}`)
+
+   if(valorUsuario > valorComputador) {
+         console.log(`O usuário venceu!`)
+      } else if (valorComputador > valorUsuario){
+         console.log(`O computador venceu!`)
+      } else 
+         console.log(`Empatou!`) 
+   } else   {
+   console.log(`Boas vindas ao jogo de Blackjack!!!`)
+   console.log(`O jogo acabou!!!`)
+}
