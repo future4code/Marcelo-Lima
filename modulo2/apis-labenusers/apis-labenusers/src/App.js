b25b10c6069e4da21d8ce0f29a0e7429efc827c3
yMarceloMaia <marcelomaiajp@gmail.com>
2021-11-18 11:01:10 -0300

    imprementando o botao delete

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,95 +1,11 @@
 import React from "react";
 import Form from "./components/Form";
 import UsersList from "./components/UsersList";
-import axios from "axios";
-import styled from "styled-components";
-
-const DivForm = styled.div`
-    border: 1px solid black;
-    display: flex;
-    flex-direction: column;
-    width: 300px;
-    margin: auto;
-    height: 200px;
-    justify-content: center;
-    align-items: center;
-    button{
-        width: 60px;
-        padding: 10px;
-        margin: 20px;
-    }
-`
-
-const DivList = styled.div`
-    text-align: center;
-`
-
 
 class App extends React.Component {
   state = {
     page: 1,
     nameButton: "lista",
-    name: "",
-    email: "",
-    users: [],
-  }
-
-
-
-
-
-  componentDidMount() {
-    this.getUsersList();
-  }
-
-  getUsersList = () => {
-    axios.get(
-      'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
-      headers: {
-        "Authorization": 'marcelo-maia-carver'
-      }
-    })
-      .then((res) => {
-        this.setState({ users: res.data })
-        // alert("ok")
-      })
-      .catch((err) => {
-        alert("erro")
-      })
-  }
-
-  createUser = () => {
-    const body = {
-      name: this.state.name,
-      email: this.state.email,
-    }
-    axios
-      .post(
-        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        body,
-        {
-          headers: {
-            Authorization: "marcelo-maia-carver"
-          }
-        }
-      )
-      .then((res) => {
-        this.setState({ name: "" })
-        this.setState({ email: "" })
-        this.getUsersList()
-      })
-      .catch((err) => {
-        console.log(err.response)
-      })
-  }
-
-  onChangeInputNome = (e) => {
-    this.setState({ name: e.target.value })
-    // console.log(this.state.name)
-  }
-
-  onChangeInputEmail = (e) => {
-    this.setState({ email: e.target.value })
   }
 
   togglePage = () => {
@@ -102,63 +18,11 @@ class App extends React.Component {
     }
   }
 
-  deleteUsers = (e) => {
-    const id = e.target.value
-    axios
-      .delete(
-        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
-        {
-          headers: {
-            Authorization: "marcelo-maia-carver"
-          },
-          data: {
-            source: id
-          }
-        }
-      ).then((answ) => {
-        alert("Usuário excluído!")
-        this.getUsersList()
-      })
-      .catch((err) => {
-        alert("Erro, tente novamente!")
-        console.log("erro:", err.response.data)
-      })
-
-  }
-
   render() {
-    
-    const listUsers = this.state.users.map((item) => {
-      return <ul><il>{item.name}</il><button value={this.state.users.id} onCLick={this.deleteUsers}>X</button></ul>
-    })
-
-    const pageRegister = <DivForm>
-      <span>
-        <label><b>Nome:</b></label>
-        <input
-          value={this.state.name}
-          onChange={this.onChangeInputNome}
-        />
-      </span>
-      <span>
-        <label><b>E-mail:</b></label>
-        <input
-          value={this.state.email}
-          onChange={this.onChangeInputEmail}
-        />
-      </span>
-      <button onClick={this.createUser} >Salvar</button>
-    </DivForm>
-
-    const pageList = <DivList>
-      <h2>Usuários cadastrados:</h2>
-      {listUsers}
-    </DivList>
-
     return (
       <div>
         <button onClick={this.togglePage}>Ir para página de {this.state.nameButton}</button>
-        {this.state.page === 1 ? pageRegister : pageList}
+        {this.state.page === 1 ? <Form /> : <UsersList />}
       </div>
     );
   }
--- modulo2/apis-labenusers/apis-labenusers/src/components/UsersList.js
+++ modulo2/apis-labenusers/apis-labenusers/src/components/UsersList.js
@@ -1,21 +1,78 @@
-// import React from "react";
-// import axios from "axios";
-// import styled from "styled-components";
-// import {Form, listUsers} from "./Form";
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
 
-// const DivList = styled.div`
-//     text-align: center;
-// `
+const DivList = styled.div`
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    ul{
+        width: 300px;
+    }
+`
 
-// class UsersList extends React.Component {
-//     render() {
-        
-//         return (
-//             <DivList>
-//                 <h2>Usuários cadastrados:</h2>
-//                 <ul></ul>
-//             </DivList>
-//         );
-//     }
-// }
-// export default UsersList;
+const DivContainerList = styled.div`
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid black;
+    padding: 5px;
+    margin: 5px;
+`
+
+class UsersList extends React.Component {
+    state = {
+        usersList: []
+    }
+
+    componentDidMount() {
+        this.getUsersList();
+    }
+
+    getUsersList = () => {
+        axios.get(
+            'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
+            headers: {
+                Authorization: "marcelo-maia-carver"
+            }
+        })
+            .then((res) => {
+                this.setState({ usersList: res.data })
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+    }
+
+    deleteUser = (id) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+        axios.delete(url, {
+            headers: {
+                Authorization: "marcelo-maia-carver"
+            }
+        })
+        .then((res) => {
+            this.getUsersList()
+            console.log("sucesso deletar", res)
+        })
+        .catch((err) => {
+            console.log("erro deletar:", err.response.data)
+        })
+    }
+    render() {
+        const userList = this.state.usersList.map((user) => {
+            return (
+                <DivContainerList key={user.id}>
+                    {user.name}
+                    <button onClick={() => this.deleteUser(user.id)}>X</button>
+                </DivContainerList>)
+        })
+        return (
+            <DivList>
+                <h2>Usuários cadastrados:</h2>
+                <ul>{userList}</ul>
+            </DivList>
+        );
+    }
+}
+export default UsersList;
