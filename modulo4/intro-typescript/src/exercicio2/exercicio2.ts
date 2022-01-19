631926309ecf871e5fa2e96c4d76fcd9b38d6232
yMarceloMaia <marcelomaiajp@gmail.com>
2022-01-19 17:49:20 -0300

    Exercicio

---
+++ modulo4/intro-typescript/src/exercicio2/exercicio2.ts
@@ -0,0 +1,10 @@
+// Exercicio 2
+// A seguinte função em JavaScript pergunta ao usuário suas três cores 
+// favoritas e imprime no console um array que contenha essas três cores. Refatore a função para o Typescript.
+
+
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string):void {
+    console.log([cor1, cor2, cor3])
+ }
+
+imprimeTresCoresFavoritas('azul', 'vermelho', 'preto')
