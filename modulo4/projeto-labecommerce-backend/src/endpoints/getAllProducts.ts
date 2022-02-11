54f602e62adb9e499de03ae4dc9670c71354616b
Marcelo Maia Lima <marcelomaiajp@gmail.com>
2022-02-11 17:32:20 -0300

    Desafio 1 feito

--- modulo4/projeto-labecommerce-backend/src/endpoints/getAllProducts.ts
+++ modulo4/projeto-labecommerce-backend/src/endpoints/getAllProducts.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from "express";
 import connection from "../data/connection";
 
-
-export const getAllProducts = async(req: Request, res: Response): Promise<any> => {
+export const getAllProducts = async (req: Request, res: Response): Promise<any> => {
     let errorCode = 500
-    try{
-        const result = await connection(`labecommerce_products`)
+    try {
+        let search = req.query.search as string
+        let order = req.query.order as string
+
+        let result = await connection(`labecommerce_products`)
+
+        if (order && order.toUpperCase() !== "ASC" && order && order.toUpperCase() !== "DESC") {
+            errorCode = 400
+            throw new Error("just values 'ASC' or 'DESC' to order")
+        }
+
+        if (order && order.toUpperCase() === "ASC" || order && order.toUpperCase() === "DESC") {
+            result = await connection(`labecommerce_products`)
+                .orderBy("name", order)
+        }
+
+        if (search) {
+            result = await connection(`labecommerce_products`)
+                .where("name", "LIKE", `%${search}%`)
+                .orderBy("name", order)
+        }
 
         res.status(201).send(result)
-    }catch(error: any){
+    } catch (error: any) {
         res.status(errorCode).send(error.message)
     }
 }
