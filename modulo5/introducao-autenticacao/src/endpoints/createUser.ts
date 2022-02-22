65906aa2137a178c42f0eac66ec393545bcbb56e
Marcelo Maia Lima <marcelomaiajp@gmail.com>
2022-02-21 22:56:35 -0300

    introdução a autenticação

---
+++ modulo5/introducao-autenticacao/src/endpoints/createUser.ts
@@ -0,0 +1,10 @@
+import connection from "../data/connection";
+
+const userTableName = "User";
+
+const createUser = async (id: string, email: string, password: string) => {
+  await connection
+    .insert({
+      id,
+      email,
+      
---
+++ modulo5/introducao-autenticacao/src/index.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import { v4 } from "uuid"
+app
+
+const id = v4();
+
+console.log("Generated Id: ", id);
+
+// Exercício 1
+// a) acho melhor pois pode se misturar letras e numeros para formar a id
+// b) 
+
+// Exercício 2
+// a) pega os parâmetros da função e utiliza para fazer um insert na tabela do banco de dados
